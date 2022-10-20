531edc77fcffd509350e77efd31ac7a1bf67822c
dubandro <dubovik@internet.ru>
2022-10-20 15:55:06 +0200

    lesson5

--- src/main/java/ru/geekbrains/Main.java
+++ src/main/java/ru/geekbrains/Main.java
@@ -8,16 +8,7 @@ public class Main {
 
         try {
             UserDao userDao = new UserDao(sessionFactoryUtils);
-            System.out.println(userDao.findById(1L));
-            System.out.println("<------------->");
 
-            userDao.update(1L, 3);
-            System.out.println(userDao.findAll());
-            System.out.println("<------------->");
-
-            userDao.delete(3L);
-            System.out.println(userDao.findAll());
-            System.out.println("<------------->");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
